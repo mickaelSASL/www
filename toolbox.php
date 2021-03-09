27afac7d70b638742900774a02d2bc1d349958b8
mickaelSASL <mickaelSASL@users.noreply.github.com>
2021-03-09 21:39:36 +0100

    Create toolbox.php

---
+++ toolbox.php
@@ -0,0 +1,114 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta content="IE=edge" http-equiv="X-UA-Compatible">
+  <meta content="width=device-width,initial-scale=1" name="viewport">
+  <meta content="Page description" name="description">
+  <meta name="google" content="notranslate" />
+  <meta content="Mashup templates have been developped by Orson.io team" name="author">
+
+  <!-- Disable tap highlight on IE -->
+  <meta name="msapplication-tap-highlight" content="no">
+  
+  <link href="images/logo_sasl.png" rel="apple-touch-icon">
+  <link href="images/favicon.ico" rel="icon">
+
+
+
+  <title>NSI - Ste Anne St Louis</title>  
+
+<link href="css/main.css" rel="stylesheet">
+</head>
+
+<body>
+
+<!-- Add your content of header -->
+<?php include("menu-side.php"); ?>
+
+
+<!-- Add your site or app content here -->
+<main class="" id="main-collapse">
+<div class="row">
+  <div class="col-xs-12 section-container-spacer">
+    <h1>Boite à outils</h1>
+ 
+  </div>
+  
+   <div class="col-xs-12 col-md-3 section-container-spacer">
+    <img class="img-responsive" alt="" src="https://hexed.it/img/hexedit-logo.png">
+    <h2>HexEdit </h2>
+	<p>Analysez et éditez les fichiers binaires où vous le souhaitez, sur n'importe quel système d'exploitation et sans installer de logiciel. </p>
+	<a href="https://hexed.it/" class="btn btn-primary" title="" target="blank"> HexEdit</a>
+  </div>
+
+  <div class="col-xs-12 col-md-3 section-container-spacer">
+    <img class="img-responsive" alt="" src="http://paletton.com/img/paletton-preview-20140414.png">
+    <h2>Paletton</h2>
+	<p>Création de palettes de couleurs pour chartes graphiques</p>
+	<a href="http://paletton.com/" class="btn btn-primary" title="" target="blank">Paletton</a>
+  </div>
+
+  <div style="height: 400px;" class="col-xs-12 col-md-3 section-container-spacer">
+    <img class="img-responsive" alt="" src="https://python.infobrisson.fr/assets/img/basthon.svg" >
+    <h2>Basthon</h2>
+	<p>Bac à sable Python - Editeur Python en ligne</p>
+	<a href="https://python.infobrisson.fr/" class="btn btn-primary" title="" target="blank">Basthon</a>
+  </div>
+  
+  <div style="height: 400px;" class="col-xs-12 col-md-3 section-container-spacer">
+    <img class="img-responsive" alt="" src="https://fiverr-res.cloudinary.com/images/t_main1,q_auto,f_auto,q_auto,f_auto/gigs/118125286/original/c2c36f39195e3e154a673be2eb5ff992e8f78a20/provide-tutoring-in-python-from-basics-to-advance.jpg" >
+    <h2>PythonTutor</h2>
+	<p>Représentation graphique des variables au cours de l'exécution d'un programme Python</p>
+	<a href="http://pythontutor.com/" class="btn btn-primary" title="" target="blank">PythonTutor</a>
+  </div>
+  
+  <div style="height: 400px;" class="col-xs-12 col-md-3 section-container-spacer">
+    <img class="img-responsive" alt="" src="http://www.frisechronos.fr/images/logo.png" >
+    <h2>FriseChronos</h2>
+	<p>Création de frises chronologiques</p>
+	<a href="http://www.frisechronos.fr/" class="btn btn-primary" title="" target="blank">FriseChronos</a>
+  </div>
+  
+    <div style="height: 400px;" class="col-xs-12 col-md-3 section-container-spacer">
+    <img class="img-responsive" alt="" src="https://fiverr-res.cloudinary.com/images/t_main1,q_auto,f_auto,q_auto,f_auto/gigs/108828277/original/45d5b55fe301234c72d78049f240c6283bf490f1/help-you-in-python-including-flask-c-sharp-html-css.png" >
+    <h2>Hilite Me</h2>
+	<p>Coloration syntaxique de code Python en HTML.</p>
+	<a href="http://hilite.me/" class="btn btn-primary" title="" target="blank">hilite me</a>
+  </div>
+  
+  
+   <div style="height: 400px;" class="col-xs-12 col-md-3 section-container-spacer">
+    <img class="img-responsive" alt="" src="https://www.picozu.com/r/images/logos/logo.png" >
+    <h2>Picozu</h2>
+	<p>Editeur d'images en ligne</p>
+	<a href="https://www.picozu.com/" class="btn btn-primary" title="" target="blank">Picozu</a>
+  </div>
+  
+  <div style="height: 400px;" class="col-xs-12 col-md-3 section-container-spacer">
+    <img class="img-responsive" alt="" src="https://pedagogie-numerique.ac-besancon.fr/wp-content/uploads/sites/49/2019/06/Une-programmer-microbit-python.png" >
+    <h2>Micro:Bit</h2>
+	<p>IDE python en ligne pour carte BBC-Micro:Bit</p>
+	<a href="https://python.microbit.org" class="btn btn-primary" title="" target="blank">Micro:Bit Python</a>
+  </div>
+  
+  <div style="height: 400px;" class="col-xs-12 col-md-3 section-container-spacer">
+    <img class="img-responsive" alt="" src="images/BBC-simul.png" >
+    <h2>Create with Code</h2>
+	<p>IDE python en ligne pour carte BBC-Micro:Bit</p>
+	<a href="https://create.withcode.uk/" class="btn btn-primary" title="" target="blank">Create with Code</a>
+  </div>
+</div> 
+ </div>
+
+<script>
+  document.addEventListener("DOMContentLoaded", function (event) {
+     masonryBuild();
+  });
+</script>
+
+</main>
+</body>
+
+</html>
