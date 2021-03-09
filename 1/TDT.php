c8cc9399616782ef9b3b0685c593d07ebf8c358b
mickaelSASL <mickaelSASL@users.noreply.github.com>
2021-03-09 20:59:29 +0100

    update

---
+++ 1/TDT.php
@@ -0,0 +1,73 @@
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta content="IE=edge" http-equiv="X-UA-Compatible">
+  <meta content="width=device-width,initial-scale=1" name="viewport">
+
+  <!-- Disable tap highlight on IE -->
+  <meta name="msapplication-tap-highlight" content="no">
+  
+  <link href="/images/favicon.ico" rel="apple-touch-icon">
+  <link href="/images/favicon.ico" rel="icon">
+
+  <title>NSI - Ste Anne St Louis</title>  
+
+
+<link href="/css/main.css" rel="stylesheet">
+<link href="/css/menu.css" rel="stylesheet">
+
+
+</head>
+<body>
+<!-- Add your content of header -->
+
+<!-- Add your site or app content here -->
+
+<main class="" id="main-collapse">
+
+<div class="col-xs-12 section-container-spacer">
+        <img  alt="" src="/images/tables.png" width="75px" style="position:relative; float:right; right: 0px; top:-30px">
+		<img  alt="" src="/images/nsi1.png" width="75px" style="position:relative; float:right; right: 0px; top:-30px"></a>
+		<h1 >Traitement des données en tables</h1>
+    </div>
+
+	<!-- Génération des articles issus du JSON -->
+
+	<div class="col-xs-12 col-md-3 section-container-spacer" style="border:solid">
+	<img class="img-responsive" alt="" src="/images/1/tables/tables.png">
+	<h2 style="height: 140px;">Titre</h2>
+	<p style="height: 80px;">Présentation</p>
+	<div class="dropdown" >
+		<div class="boutonmenuprincipal">Menu</div>				
+		<div class="dropdown-child">
+		<a href="menu1" target="_blank">menu1</a>
+		
+<!-- Pied de page avec les légendes -->
+<div class="legende_icone">		
+	<div>Légende :</div>
+	<div><i class='material-icons'>menu_book</i> : Cours </div>
+	<div><i class='material-icons'>rate_review</i> : Exercices</div>
+	<div><i class='material-icons'>work</i> : Projet</div>
+	<div><i class='material-icons'>local_library</i> : Documents ressources</div>
+	<div><i class='material-icons'>open_in_browser</i> : Ressources web</div>
+	<div><i class='material-icons'>play_circle_outline</i> : Vidéos</div>
+	<div><i class='material-icons'>question_answer</i> : QCM</div>
+	
+</div>	
+	
+</main>
+
+
+<script>
+	document.addEventListener("DOMContentLoaded", function (event) {
+	  navbarToggleSidebar();
+	  navActivePage();
+	});
+</script>
+
+<script type="text/javascript" src="/js/main.js"></script></body>
+</body>
+</html>
