71380139d746d9d5cae5279bca2770a0656ec003
mickaelSASL <mickaelSASL@users.noreply.github.com>
2021-03-09 16:49:50 +0100

    Update index.php

--- T/index.php
+++ T/index.php
@@ -13,114 +13,26 @@
   <!-- Disable tap highlight on IE -->
   <meta name="msapplication-tap-highlight" content="no">
   
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-  <link href="/images/favicon.ico" rel="apple-touch-icon">
-  <link href="/images/favicon.ico" rel="icon">
-=======
-  <link href="images/favicon.ico" rel="apple-touch-icon">
-  <link href="images/favicon.ico" rel="icon">
->>>>>>> 52b538c (update)
-=======
-  <link href="/images/favicon.ico" rel="apple-touch-icon">
-  <link href="/images/favicon.ico" rel="icon">
->>>>>>> 4bdc710 (update)
-=======
-  <link href="images/favicon.ico" rel="apple-touch-icon">
-  <link href="images/favicon.ico" rel="icon">
->>>>>>> c6a503c (Update index.php)
-=======
-  <link href="/images/favicon.ico" rel="apple-touch-icon">
-  <link href="/images/favicon.ico" rel="icon">
->>>>>>> 832a32e (Update index.php)
-=======
-  <link href="./images/favicon.ico" rel="apple-touch-icon">
-  <link href="./images/favicon.ico" rel="icon">
->>>>>>> 699a714 (Update index.php)
-=======
-  <link href="/images/favicon.ico" rel="apple-touch-icon">
-  <link href="/images/favicon.ico" rel="icon">
->>>>>>> 724a7b6 (Update index.php)
 
-
+  <link href="/images/favicon.ico" rel="apple-touch-icon">
+  <link href="/images/favicon.ico" rel="icon">
 
   <title>NSI - Ste Anne St Louis</title>  
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
 <link href="/css/main.css" rel="stylesheet">
 <script src="/js/menu.js"></script>
-=======
-<link href="css/main.css" rel="stylesheet">
-<script src="js/menu.js"></script>
->>>>>>> 52b538c (update)
-=======
-<link href="/css/main.css" rel="stylesheet">
-<script src="/js/menu.js"></script>
->>>>>>> 4bdc710 (update)
-=======
-<link href="css/main.css" rel="stylesheet">
-<script src="js/menu.js"></script>
->>>>>>> c6a503c (Update index.php)
-=======
-<link href="/css/main.css" rel="stylesheet">
-<script src="/js/menu.js"></script>
->>>>>>> 832a32e (Update index.php)
-=======
-<link href="./css/main.css" rel="stylesheet">
-<script src="./js/menu.js"></script>
->>>>>>> 699a714 (Update index.php)
-=======
-<link href="/css/main.css" rel="stylesheet">
-<script src="/js/menu.js"></script>
->>>>>>> 724a7b6 (Update index.php)
+
 </head>
 
 <body>
-
-	<!-- Add your content of header -->
-
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-	<?php include('../menu-side.php'); ?>
-=======
-	<?php include('menu-side.php'); ?>
->>>>>>> 52b538c (update)
-=======
-	<?php include('/menu-side.php'); ?>
->>>>>>> 832a32e (Update index.php)
-=======
-	<?php include('./menu-side.php'); ?>
->>>>>>> 699a714 (Update index.php)
-=======
-	<?php include('../menu-side.php'); ?>
->>>>>>> c9a2b9a (update)
-
 
 
 	<!-- Add your site or app content here -->
 	<main class="" id="main-collapse">
 
 		<div class="section-container-spacer">
-<<<<<<< HEAD
-<<<<<<< HEAD
 			<img  alt="" src="/images/nsiT.png" width="75px" style="position:relative; float:right; right: 0px; top:-30px">
-=======
-			<img  alt="" src="images/nsiT.png" width="75px" style="position:relative; float:right; right: 0px; top:-30px">
->>>>>>> 52b538c (update)
-=======
-			<img  alt="" src="/images/nsiT.png" width="75px" style="position:relative; float:right; right: 0px; top:-30px">
->>>>>>> c9a2b9a (update)
+
 				<h1>Terminale</h1>
 				<div class="hero-full-wrapper section-container-spacer">
 					<div class="grid">
@@ -129,21 +41,10 @@
 
 
 						<div class="grid-item">
-<<<<<<< HEAD
-<<<<<<< HEAD
+
 							<img class="img-responsive" alt="" src="/images/histoire.png">
-=======
-							<img class="img-responsive" alt="" src="images/histoire.png">
-<<<<<<< HEAD
->>>>>>> 52b538c (update)
-							<a href="theme.php?theme=histoire&niveau='.$niveau.'" class="project-description">
-=======
 							<a href="HI.php" class="project-description">
->>>>>>> c6a503c (Update index.php)
-=======
-							<img class="img-responsive" alt="" src="/images/histoire.png">
-							<a href="theme.php?theme=histoire&niveau='.$niveau.'" class="project-description">
->>>>>>> c9a2b9a (update)
+
 								<div class="project-text-holder">
 									<div class="project-text-inner">
 										<h3>1 - Histoire de l'informatique</h3>
@@ -154,21 +55,12 @@
 						</div>
 						
 						<div class="grid-item">
-<<<<<<< HEAD
-<<<<<<< HEAD
+
 							<img class="img-responsive" alt="" src="/images/donnees.png">
-=======
-							<img class="img-responsive" alt="" src="images/donnees.png">
-<<<<<<< HEAD
->>>>>>> 52b538c (update)
-							<a href="theme.php?theme=struc_donnees&niveau=T" class="project-description">
-=======
+
+
 							<a href="SD.php" class="project-description">
->>>>>>> c6a503c (Update index.php)
-=======
-							<img class="img-responsive" alt="" src="/images/donnees.png">
-							<a href="theme.php?theme=struc_donnees&niveau=T" class="project-description">
->>>>>>> c9a2b9a (update)
+
 								<div class="project-text-holder">
 									<div class="project-text-inner">
 										<h3>2 - Structures de données</h3>
@@ -179,21 +71,11 @@
 						</div>
 
 						<div class="grid-item">
-<<<<<<< HEAD
-<<<<<<< HEAD
+
 							<img class="img-responsive" alt="" src="/images/bdd.png">
-=======
-							<img class="img-responsive" alt="" src="images/bdd.png">
-<<<<<<< HEAD
->>>>>>> 52b538c (update)
-							<a href="theme.php?theme=bdd&niveau=T" class="project-description">
-=======
+
 							<a href="BDD.php" class="project-description">
->>>>>>> c6a503c (Update index.php)
-=======
-							<img class="img-responsive" alt="" src="/images/bdd.png">
-							<a href="theme.php?theme=bdd&niveau=T" class="project-description">
->>>>>>> c9a2b9a (update)
+
 								<div class="project-text-holder">
 									<div class="project-text-inner">
 										<h3>3 - Bases de données</h3>
@@ -204,21 +86,11 @@
 						</div>
 
 						<div class="grid-item">
-<<<<<<< HEAD
-<<<<<<< HEAD
+
 							<img class="img-responsive" alt="" src="/images/archi.png" >
-=======
-							<img class="img-responsive" alt="" src="images/archi.png" >
-<<<<<<< HEAD
->>>>>>> 52b538c (update)
-							<a href="theme.php?theme=archi&niveau='.$niveau.'" class="project-description">
-=======
+
 							<a href="AMSER.php" class="project-description">
->>>>>>> c6a503c (Update index.php)
-=======
-							<img class="img-responsive" alt="" src="/images/archi.png" >
-							<a href="theme.php?theme=archi&niveau='.$niveau.'" class="project-description">
->>>>>>> c9a2b9a (update)
+
 								<div class="project-text-holder">
 									<div class="project-text-inner">
 										<h3>4 - Architectures matérielles, systèmes d’exploitation et réseaux</h3>
@@ -229,21 +101,11 @@
 						</div>
 
 						<div class="grid-item">
-<<<<<<< HEAD
-<<<<<<< HEAD
+
 							<img class="img-responsive" alt="" src="/images/prog.png">
-=======
-							<img class="img-responsive" alt="" src="images/prog.png">
-<<<<<<< HEAD
->>>>>>> 52b538c (update)
-							<a href="theme.php?theme=prog&niveau='.$niveau.'" class="project-description">
-=======
+
 							<a href="LP.php'" class="project-description">
->>>>>>> c6a503c (Update index.php)
-=======
-							<img class="img-responsive" alt="" src="/images/prog.png">
-							<a href="theme.php?theme=prog&niveau='.$niveau.'" class="project-description">
->>>>>>> c9a2b9a (update)
+
 								<div class="project-text-holder">
 									<div class="project-text-inner">
 										<h3>5 - Langages et programmation</h3>
@@ -254,21 +116,10 @@
 						</div>
 
 						<div class="grid-item">
-<<<<<<< HEAD
-<<<<<<< HEAD
+
 							<img class="img-responsive" alt="" src="/images/algo.png">
-=======
-							<img class="img-responsive" alt="" src="images/algo.png">
-<<<<<<< HEAD
->>>>>>> 52b538c (update)
-							<a href="theme.php?theme=algo&niveau='.$niveau.'" class="project-description">
-=======
 							<a href="A.php" class="project-description">
->>>>>>> c6a503c (Update index.php)
-=======
-							<img class="img-responsive" alt="" src="/images/algo.png">
-							<a href="theme.php?theme=algo&niveau='.$niveau.'" class="project-description">
->>>>>>> c9a2b9a (update)
+
 								<div class="project-text-holder">
 									<div class="project-text-inner">
 										<h3>6 - Algorithmique</h3>
@@ -279,21 +130,10 @@
 						</div>
 
 						<div class="grid-item">
-<<<<<<< HEAD
-<<<<<<< HEAD
+
 							<img class="img-responsive" alt="" src="/images/projet.png">
-=======
-							<img class="img-responsive" alt="" src="images/projet.png">
-<<<<<<< HEAD
->>>>>>> 52b538c (update)
-							<a href="theme.php?theme=projet&niveau='.$niveau.'" class="project-description">
-=======
+
 							<a href="P.php" class="project-description">
->>>>>>> c6a503c (Update index.php)
-=======
-							<img class="img-responsive" alt="" src="/images/projet.png">
-							<a href="theme.php?theme=projet&niveau='.$niveau.'" class="project-description">
->>>>>>> c9a2b9a (update)
 								<div class="project-text-holder">
 									<div class="project-text-inner">
 										<h3>7 - Démarche de projets</h3>
@@ -319,14 +159,6 @@
      masonryBuild();
   });
 </script>
-<<<<<<< HEAD
-<<<<<<< HEAD
 <script type="text/javascript" src="/js/main.js"></script>
-=======
-<script type="text/javascript" src="js/main.js"></script>
->>>>>>> 52b538c (update)
-=======
-<script type="text/javascript" src="/js/main.js"></script>
->>>>>>> c9a2b9a (update)
 </body>
 </html>
