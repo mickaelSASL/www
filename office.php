aca082609c11191867e2f97eaf13e7ce23016cdb
mickaelSASL <mickaelSASL@users.noreply.github.com>
2021-03-09 21:37:48 +0100

    Create office.php

---
+++ office.php
@@ -0,0 +1,7 @@
+<?php
+$actual_link = "http://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
+include("https://view.officeapps.live.com/op/view.aspx?src=".$actual_link."SD4-Arbres.docx");
+
+#include("https://colab.research.google.com/github/mickaelSASL/mickaelSASL.github.io/blob/master/T/2-Structures de données/Untitled 2020-09-26 19_01_02.ipynb");
+
+?>
